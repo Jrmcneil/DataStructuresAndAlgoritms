24755fa0ee3361ea518e1b5ae194cf32f6976ca3
James McNeil <james.r.mcneil@gmail.com>
2016-08-17 14:08:38 +0100

    adjusts indentation

--- src/Chapter2/OrdArray.java
+++ src/Chapter2/OrdArray.java
@@ -5,36 +5,36 @@ package Chapter2;
  * demonstrates ordered array class
  */
 public class OrdArray {
-    private long[] a;
-    private int nElems;
+  private long[] a;
+  private int nElems;
 
-    public OrdArray(int max) {
-        a = new long[max];
-        nElems = 0;
-    }
+  public OrdArray(int max) {
+    a = new long[max];
+    nElems = 0;
+  }
 
-    public int size() {
+  public int size() {
+    return nElems;
+  }
+
+  public int find(long searchKey) {
+    int lowerBound = 0;
+    int upperBound = nElems - 1;
+    int curIn;
+
+    while(true) {
+      curIn = (lowerBound + upperBound) / 2;
+      if(a[curIn] == searchKey) {
+        return curIn;
+      } else if(lowerBound > upperBound) {
         return nElems;
-    }
-
-    public int find(long searchKey) {
-        int lowerBound = 0;
-        int upperBound = nElems - 1;
-        int curIn;
-
-        while(true) {
-            curIn = (lowerBound + upperBound) / 2;
-            if(a[curIn] == searchKey) {
-                return curIn;
-            } else if(lowerBound > upperBound) {
-                return nElems;
-            } else {
-                if(a[curIn] < searchKey) {
-                    lowerBound = curIn + 1;
-                } else {
-                    upperBound = curIn - 1
-                }
-            }
+      } else {
+        if(a[curIn] < searchKey) {
+          lowerBound = curIn + 1;
+        } else {
+          upperBound = curIn - 1
         }
+      }
     }
+  }
 }
